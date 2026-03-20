0ad1447e8f55d1d66c640acd927527e3b408519a
dreamyellow1703-ops <dreamyellow1703@gmail.com>
2026-03-20 18:12:53 +0530

    Add files via upload

---
+++ guess the number.py
@@ -0,0 +1,15 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+===== RESTART: C:\Users\Jaskaran Singh\OneDrive\文档\Number Guessing game.py =====
+I'm thinking of a number between 1 and 100. Can you guess it?
+Enter your guess: 90
+Too high! Try again.
+Enter your guess: 40
+Too low! Try again.
+Enter your guess: 50
+Too low! Try again.
+Enter your guess: 60
+Too high! Try again.
+Enter your guess: 57
+Congratulations! You guessed it in 5 attempts.
